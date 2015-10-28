91184fa6af2af18aa0f95cd25701bc5ff77aaf07
Vasil Emilov <vnemilov@yahoo.com>
2015-10-28 10:44:56 +0200

    branch1 string

--- src/com/sefas/testone/GreetingGenerator.java
+++ src/com/sefas/testone/GreetingGenerator.java
@@ -9,6 +9,7 @@ public class GreetingGenerator {
 	}
 
 	public String generate(Person person) {
+		String branch1 = "This is branch1";
 		String issue = "This is string for the new branch";
 		int c = 10;
 		int a = 1555;
